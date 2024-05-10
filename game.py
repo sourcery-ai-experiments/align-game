4004348a05e03e48d81603e5040b1eacfea7891e
bugston <bugstonn@gmail.com>
2024-05-10 09:34:29 +0300

    buttons react to being clicked and print the acording word for what they do

--- game.py
+++ game.py
@@ -219,6 +219,44 @@ class AlignIt:
 
     #     return buttona
 
+    def quit_menu(
+        self,
+        text,
+        load_button_text,
+        reset_button_text,
+        quit_button_text,
+        color, x, y,
+    ):
+        screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+        img = self.text_font.render(text, True, color)
+        score_text = self.text_font.render(
+            f'Score: {self.scoreall}', True, WHITE,
+        )
+        moves = self.text_font.render(
+            f'Moves made: {self.moves_made}', True, WHITE,
+        )
+        screen.blit(img, (x, y))
+        screen.blit(score_text, (x, y + 50))
+        screen.blit(moves, (x, y + 100))
+        load_button_rect = pygame.Rect(x, y + 270, 150, 50)
+        self.render_reset_button(screen, load_button_rect, load_button_text)
+        reset_button_rect = pygame.Rect(x, y + 210, 150, 50)
+        self.render_reset_button(screen, reset_button_rect, reset_button_text)
+        quit_button_rect = pygame.Rect(x, y + 150, 150, 50)
+        self.render_quit_button(screen, quit_button_rect, quit_button_text)
+        pygame.display.update()
+
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    mouse_pos = pygame.mouse.get_pos()
+                    if quit_button_rect.collidepoint(mouse_pos):
+                        self.load_function()
+                    elif reset_button_rect.collidepoint(mouse_pos):
+                        self.reset_function()
+                    elif load_button_rect.collidepoint(mouse_pos):
+                        self.quit_function()
+
     def render_quit_button(self, screen, button_rect, quit_button_text):
         pygame.draw.rect(screen, GREY, button_rect)
         button_text_surface = self.text_font.render(
@@ -243,32 +281,18 @@ class AlignIt:
         text_rect = button_text_surface.get_rect(center=button_rect.center)
         screen.blit(button_text_surface, text_rect)
 
-    def quit_menu(
-            self,
-            text,
-            quit_button_text,
-            reset_button_text,
-            load_button_text,
-            color, x, y,
-    ):
-        screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-        img = self.text_font.render(text, True, color)
-        score_text = self.text_font.render(
-            f'Score: {self.scoreall}', True, WHITE,
-        )
-        moves = self.text_font.render(
-            f'Moves made: {self.moves_made}', True, WHITE,
-        )
-        screen.blit(img, (x, y))
-        screen.blit(score_text, (x, y + 50))
-        screen.blit(moves, (x, y + 100))
-        quit_button_rect = pygame.Rect(x, y + 150, 150, 50)
-        self.render_quit_button(screen, quit_button_rect, quit_button_text)
-        reset_button_rect = pygame.Rect(x, y + 210, 150, 50)
-        self.render_reset_button(screen, reset_button_rect, reset_button_text)
-        load_button_rect = pygame.Rect(x, y + 270, 150, 50)
-        self.render_reset_button(screen, load_button_rect, load_button_text)
-        pygame.display.update()
+    def quit_function(self):
+        print('quit')
+        pygame.quit()
+        quit()
+
+    def reset_function(self):
+        print('reset')
+        pass
+
+    def load_function(self):
+        print('load')
+        pass
 
     def game_over(self, text, color, x, y):
         screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
