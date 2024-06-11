3c87e8d4f9dae30f17313b6d8f511f45bb761499
bugston <bugstonn@gmail.com>
2024-06-11 15:57:35 +0300

    logical grid finaly! updates acordingly

--- main.py
+++ main.py
@@ -21,30 +21,54 @@ COLOR_LIST = [
 
 class MyPaintApp(App):
 
-    def on_button_press(self, instance, row, col):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.logical_grid = [[0 for _ in range(9)] for _ in range(9)]
+        self.selected_color = None
+        self.selected_button = None
+        self.grid_layout = None
+
+    def on_button_press(self, tile, row, col):
         print(row, col)
-        if instance.background_color != [1, 1, 1, 1]:
+        if tile.background_color != [1, 1, 1, 1]:
+            self.selected_color = tile.background_color
+            self.selected_button = tile
+            self.update_logical_grid(row, col, 0)
             print('colored')
+
+        elif tile.background_color == [
+            1, 1, 1, 1,
+        ] and self.selected_color is not None:
+            tile.background_color = self.selected_color
+            self.update_logical_grid(row, col, 1)
+            self.selected_button.background_color = [1, 1, 1, 1]
+            self.selected_color = None
+            self.selected_button = None
+        print(self.logical_grid[0])
+
+    def update_logical_grid(self, row, col, value):
+        self.logical_grid[row][col] = value
 
     def assign_random_colors_to_buttons(self, buttons, colors):
         for btn, color in zip(buttons, colors):
             btn.background_color = color
+            btn.logical_color = 1
 
     def build_grid_layout(self):
-        grid_layout = GridLayout(
+        self.grid_layout = GridLayout(
             cols=9, rows=9,
             size_hint=(None, None),
             size=(450, 450), pos=(330, 20),
         )
         for row in range(9):
             for col in range(9):
-                btn = Button()
+                btn = Button(background_color=[1, 1, 1, 1])
                 btn.bind(
                     on_press=lambda instance, x=row,
                     y=col: self.on_button_press(instance, x, y),
                 )
-                grid_layout.add_widget(btn)
-        return grid_layout
+                self.grid_layout.add_widget(btn)
+        return self.grid_layout
 
     def future_grid(self):
         button_layout = BoxLayout(
